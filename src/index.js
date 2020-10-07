ebcad8d844065acbaa9c0cda9bb6769f92611d56
Elwa36s <24dds08@gmail.com>
2020-10-07 15:38:09 +0300

    feat: add numbers from 0 to 13

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable (number) {
-let numberSymb = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+let numberSymb = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     numberName = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'],
     result = '';
     if (number <= 12) {
@@ -12,6 +12,4 @@ let numberSymb = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
         result = `${numberName[j]}teen`;
     }
     return result;
-
 }
-console.log(toReadable(4));
